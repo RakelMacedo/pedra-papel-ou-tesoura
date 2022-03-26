3249ff41d14f937bfd9df2fbed4cb70639f3efbe
Rakel Macedo <macedorakel35@gmail.com>
2022-03-26 02:51:54 -0300

    first commit

---
+++ dados.py
@@ -0,0 +1,13 @@
+boas_vindas = ("\nOlá, seja bem-vindo ao jogo de pedra papel ou tesoura!")
+
+opcoes = ["pedra", "papel", "tesoura"]
+
+# Variáveis do Placar
+pontos_robo = 0
+pontos_jogador = 0
+
+escolha = ("\nPor favor, digite a sua escolha:\npedra, papel, tesoura ou sair?\n\n>> ")
+
+agradecimento = ("\nFicamos felizes com a sua presença! Volte sempre! <3\n")
+
+erro = ("\nOpa! Identificamos um erro de escrita, tente novamente.")
---
+++ main.py
@@ -0,0 +1,51 @@
+from dados import boas_vindas, opcoes, pontos_robo, pontos_jogador, escolha, agradecimento, erro
+import random
+
+print(boas_vindas)
+
+while True:
+    jogador = input(escolha).lower() # Opção do jogador
+    robo = random.choice(opcoes) # Escolha aleatória do robô 
+
+    if jogador in opcoes:
+        print("\nVocê escolheu " + jogador) # Output das oções do jogador
+        print("Eu escolho " + robo) # Output das oções do robô
+        
+        if jogador == robo: # Empate
+            print("\nEmpate! :/\n")
+        
+        elif jogador == "papel": # PAPEL
+            if robo == "tesoura":
+                print("\nEu venci! Minha tesoura cortou seu papel :D\n")
+                pontos_robo += 1
+            else: # robo == "pedra"
+                print("\nVocê venceu! Seu papel cobriu minha pedra :(\n")
+                pontos_jogador += 1
+        
+        elif jogador == "tesoura": # TESOURA
+            if robo == "papel":
+                print("\nVocê venceu! Sua tesoura cortou meu papel :(\n")
+                pontos_jogador += 1
+            else: # robo == "pedra"
+                print("\nEu venci! Minha pedra quebrou sua tesoura :D\n")
+                pontos_robo += 1
+        
+        elif jogador == "pedra": # PEDRA
+            if robo == "papel":
+                print("\nEu venci! Meu papel enrolou sua pedra :D\n")
+                pontos_robo += 1
+            else: # robo == tesoura
+                print("\nVocê venceu! Sua pedra quebrou minha tesoura :(\n")
+                
+                pontos_jogador += 1
+        print("---------PLACAR---------")
+        print("EU: " + str(pontos_jogador))
+        print("Robô: " + str(pontos_robo))
+        print("------------------------")
+    
+    elif jogador == "sair":
+        print(agradecimento)
+        break
+    
+    else:
+        print(erro)
