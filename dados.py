b8a76efa69f7d7d8da9c2cec6fbd92191cb32429
Rakel Macedo <78339857+RakelMacedo@users.noreply.github.com>
2022-03-26 03:07:40 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,13 +1,18 @@
+# Mensagem de boas-vindas
 boas_vindas = ("\nOlá, seja bem-vindo ao jogo de pedra papel ou tesoura!")
 
+# Opções do jogo
 opcoes = ["pedra", "papel", "tesoura"]
 
 # Variáveis do Placar
 pontos_robo = 0
 pontos_jogador = 0
 
+# Mensagem para a seleção da escolha entre pedra papel ou tesoura
 escolha = ("\nPor favor, digite a sua escolha:\npedra, papel, tesoura ou sair?\n\n>> ")
 
+# Mensagem de agradecimento
 agradecimento = ("\nFicamos felizes com a sua presença! Volte sempre! <3\n")
 
+# Mensagem de erro
 erro = ("\nOpa! Identificamos um erro de escrita, tente novamente.")
